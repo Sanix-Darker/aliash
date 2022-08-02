d65fe9ccbd018fe9c0d406d750ed23b1c20eca26
sanix-darker <s4nixd@gmail.com>
2022-08-02 05:09:45 +0200

    wip: WIP mongoDB integration

---
+++ db.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+var MONGODB_URL = os.Getenv("MONGODB_URL")
+
+var DATABASE = os.Getenv("DATABASE")
+
+func init() {
+	clientOptions := options.Client().ApplyURI(MONGODB_URL)
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	Must(err)
+
+	Must(client.Ping(ctx, nil))
+
+	collection = client.Database(DATABASE).Collection("aliases")
+}
+
+// createAliases to create a new alias
+func createAliases(as *Aliases) error {
+	_, err := collection.InsertOne(ctx, as)
+	return err
+}
+
+// getAllAliases To get all aliases
+// passing bson.D{{}} matches all documents in the collection
+func getAllAliases() ([]*Aliases, error) {
+	filter := bson.D{{}}
+	return getAliasessBy(filter)
+}
+
+// getAliasess to get aliases from mongodb
+// with a given filter sjon filter
+// A slice of aliases for storing the decoded documents
+func getAliasessBy(filter interface{}) ([]*Aliases, error) {
+	var aliases []*Aliases
+
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return aliases, err
+	}
+
+	for cur.Next(ctx) {
+		var t Aliases
+		err := cur.Decode(&t)
+		if err != nil {
+			return aliases, err
+		}
+
+		aliases = append(aliases, &t)
+	}
+
+	if err := cur.Err(); err != nil {
+		return aliases, err
+	}
+
+	// once exhausted, close the cursor
+	cur.Close(ctx)
+
+	if len(aliases) == 0 {
+		return aliases, mongo.ErrNoDocuments
+	}
+
+	return aliases, nil
+}
--- models.go
+++ models.go
@@ -1,9 +1,31 @@
 package main
 
-type Alias struct {
-	ID          string `bson:"ID"`
-	uid         string `bson:"uid"`
-	url         string `bson:"url"`
-	author      string `bson:"author"`
-	description string `bson:"description"`
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Aliases isa raw alias defined by an uid and a raw_url where to fetch
+type Aliases struct {
+	ID          primitive.ObjectID `bson:"_id"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
+	Uid         string             `bson:"uid"`
+	RawUrl      string             `bson:"raw_url"`
+	Description string             `bson:"description"`
+}
+
+// Link define the link between analias an it user
+type Link struct {
+	ID     primitive.ObjectID `bson:"_id"`
+	Alias  Aliases            `bson:"alias"`
+	Author User               `bson:"author"`
+}
+
+type User struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Pseudo        string             `bson:"pseudo"`
+	GithubProfile string             `bson:"github_profile"`
+	CreatedAt     time.Time          `bson:"created_at"`
 }
