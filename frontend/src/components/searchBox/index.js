1ab982308321eabf3baa3b3bbbe3c69d38c24667
sanix-darker <s4nixd@gmail.com>
2022-08-07 04:07:09 +0200

    feat: small refacto of the searchBox

--- frontend/src/components/searchBox/index.js
+++ frontend/src/components/searchBox/index.js
@@ -1,18 +1,39 @@
+import { Link } from 'preact-router/match';
 import {useState} from "preact/hooks";
 import {dataSearchAliases} from "../../data";
 import './style.css';
 
 
+
+const SearchItem = ({key, item}) => {
+
+    return <Link href={`/details/${item.Uid}`}>
+        <li key={key} className="item">{item?.Title}</li>
+    </Link>
+}
+
+const SearchListItem = ({searchList, query}) => {
+
+    return searchList.length > 0 && query.length > 0 ? (
+            <div className="result-block">
+                <ul> {searchList.map((item, key) => <SearchItem key={key} item={item} />)} </ul>
+            </div>
+    ): null
+}
+
 const SearchBox = () => {
+
     const [query, setQuery] = useState('');
     const [searchList, setSearchList] = useState([])
 
     const handleAutoCompletion = async (searchValue) => {
-
         setQuery(searchValue);
         if (searchValue.length > 1){
-            const res = await dataSearchAliases(searchValue);
-            setSearchList(res);
+            // added setimeout to split amout of backend calls
+            setTimeout(async () => {
+                const res = await dataSearchAliases(searchValue);
+                setSearchList(res);
+            }, 500);
         }
     }
 
@@ -25,11 +46,7 @@ const SearchBox = () => {
             defaultValue={query}
             onKeyUp={(e) => handleAutoCompletion(e.target.value)}
         />
-        {searchList.length > 0 && query.length > 0 ? (
-            <div className="result-block">
-                <ul> {searchList.map((r, i) => <li key={i} className="item">{r?.Title}</li>)} </ul>
-            </div>
-        ): null}
+        <SearchListItem searchList={searchList} query={query} />
     </>;
 }
 
