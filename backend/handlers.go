175696041b2e47b68512c3ae5bfa30b1c0edddf7
darker <s4nixd@gmail.com>
2022-09-17 10:51:36 +0200

    feat: remove unecessary header patch (#5)
    * feat: remove unecessary header patch
    
    * add return statement after every error response render
    
    Co-authored-by: Aser <aserdipanda@gmail.com>

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/gosimple/slug"
 	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -29,7 +29,6 @@ func CORSMiddleware() gin.HandlerFunc {
 }
 
 func HomeHandler(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
 		"status":  "success",
 		"message": "aliash is up and running !",
@@ -38,7 +37,6 @@ func HomeHandler(c *gin.Context) {
 }
 
 func InstallHandler(c *gin.Context) {
-
 	installScript, err := os.ReadFile("./install.sh")
 	Must(err)
 
@@ -46,8 +44,6 @@ func InstallHandler(c *gin.Context) {
 }
 
 func CreateAliasesHandler(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "application/json")
-
 	var as Aliases
 
 	Must(c.BindJSON(&as))
@@ -56,6 +52,7 @@ func CreateAliasesHandler(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Your content should not be empty",
 		})
+		return
 	}
 
 	// We should check here if the user is logged, then allow content <= 1000
@@ -63,95 +60,91 @@ func CreateAliasesHandler(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Your content is > 300",
 		})
-	} else {
-		as.Title = TruncateText(as.Title, 70)
-		// we truncate to 300 characters for the markdown description of the
-		as.Description = TruncateText(as.Description, 300)
-
-		// We should search for the same title and the content in the database
-		// and refuse to save if there is already something similar in the database
-		filter := bson.M{"$and": []interface{}{bson.M{"title": as.Title}, bson.M{"content": as.Content}}}
-
-		aliases, _ := filterAliasesBy(filter)
-
-		if len(aliases) > 0 {
-			c.JSON(http.StatusAlreadyReported, gin.H{
-				"error": "An alias already exist for this title and this content",
-			})
-		} else {
-			as.Hash512 = ShaIt(as.Title + as.Content)
-			as.Uid = TruncateText(slug.Make(as.Title), 2) + "-" + TruncateText(as.Hash512, 7)
-			as.CreatedAt = time.Now()
-			as.UpdatedAt = time.Now()
-
-			if err := createAliases(&as); err != nil {
-				c.JSON(http.StatusForbidden, gin.H{
-					"error": err,
-				})
-			} else {
-				c.JSON(http.StatusOK, gin.H{
-					"uid":        as.Uid,
-					"title":      as.Title,
-					"hash":       TruncateText(as.Hash512, 4),
-					"created_at": as.CreatedAt,
-				})
-			}
-		}
+		return
 	}
+
+	as.Title = TruncateText(as.Title, 70)
+	// we truncate to 300 characters for the markdown description of the
+	as.Description = TruncateText(as.Description, 300)
+
+	// We should search for the same title and the content in the database
+	// and refuse to save if there is already something similar in the database
+	filter := bson.M{"$and": []interface{}{bson.M{"title": as.Title}, bson.M{"content": as.Content}}}
+
+	aliases, _ := filterAliasesBy(filter)
+
+	if len(aliases) > 0 {
+		c.JSON(http.StatusAlreadyReported, gin.H{
+			"error": "An alias already exist for this title and this content",
+		})
+		return
+	}
+
+	as.Hash512 = ShaIt(as.Title + as.Content)
+	as.Uid = TruncateText(slug.Make(as.Title), 2) + "-" + TruncateText(as.Hash512, 7)
+	as.CreatedAt = time.Now()
+	as.UpdatedAt = time.Now()
+
+	if err := createAliases(&as); err != nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"uid":        as.Uid,
+		"title":      as.Title,
+		"hash":       TruncateText(as.Hash512, 4),
+		"created_at": as.CreatedAt,
+	})
 }
 
 func SearchHandler(c *gin.Context) {
-
 	searchText, status := c.GetQuery("q")
 
 	if !status {
 		c.JSON(http.StatusOK, gin.H{
 			"aliases": []*Aliases{},
 		})
-	} else {
-		aliases, err := searchAliases("title", searchText)
-		Must(err)
-		if aliases == nil {
-			aliases = []*Aliases{}
-		}
-
-		c.JSON(http.StatusOK, aliases)
+		return
 	}
+
+	aliases, err := searchAliases("title", searchText)
+	Must(err)
+	if aliases == nil {
+		aliases = []*Aliases{}
+	}
+
+	c.JSON(http.StatusOK, aliases)
 }
 
 func GetAllHandler(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "application/json")
-
 	aliases := getAllAliases()
 
 	c.JSON(http.StatusOK, aliases)
 }
 
 func GetHandler(c *gin.Context) {
-	c.Writer.Header().Set("Content-Type", "application/json")
-
 	uid := c.Param("uid")
 
-	if len(uid) > 0 {
-		filter := bson.D{{"uid", uid}}
-		aliases, err := filterAliasesBy(filter)
-
-		if err != nil || len(aliases) == 0 {
-			log.Printf("%s", err)
-
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "No aliases found for this uid !",
-			})
-		} else {
-			content := aliases[0].Content
-
-			c.Data(http.StatusOK, "text/plain", []byte(content))
-		}
-	} else {
+	if strings.TrimSpace(uid) == "" {
 		log.Printf("[x] uid is empty !")
-
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "You need to provide the uid parameter !",
 		})
+		return
 	}
+
+	filter := bson.D{{"uid", uid}}
+	aliases, err := filterAliasesBy(filter)
+	if err != nil || len(aliases) == 0 {
+		log.Printf("%s", err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "No aliases found for this uid !",
+		})
+		return
+	}
+
+	c.Data(http.StatusOK, "text/plain", []byte(aliases[0].Content))
 }
